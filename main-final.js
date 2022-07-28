9b7b1b7a1e94a4d2a778a50d8863ecd7a23e7142
adeliat <adelia2000016077@webmail.uad.ac.id>
2022-07-29 01:10:40 +0700

    update json

---
+++ main-final.js
@@ -0,0 +1,39 @@
+Vue.createApp({
+    data() {
+      return {
+        message: "Hello Vue!",
+        header: {},
+        articles: []
+      };
+    },
+    methods:{    
+        getHeaderData()
+        {
+          axios
+            .get("https://raw.githubusercontent.com/adeliat/tekweb2022/main/header.json")
+            .then((res) => {              
+              this.header = res.data;
+              this.getArticles();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        },
+        getArticles()
+        {
+            axios
+            .get(
+              "https://raw.githubusercontent.com/faridsurya/Vue-Axios-Basic/master/contents/articles.json"
+            )
+            .then((res) => {              
+              this.articles = res.data;
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+    },
+    beforeMount() {
+        this.getHeaderData()
+    }
+  }).mount("#app");
